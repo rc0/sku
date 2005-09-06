d3024507f50fb803dbb6f154bcd92945a4c62703
Richard P. Curnow <rc@rc0.org.uk>
2005-09-07 00:13:25 +0100

    Add GPL headers to source files + add COPYING Signed-off-by: Richard P. Curnow <rc@rc0.org.uk>

--- sku.c
+++ sku.c
@@ -1,10 +1,20 @@
-
-/* TODO:
- * symmetrical removal of givens in pose mode
- * tidy up verbose output
- * hint mode : advance to the next allocate in a part-solved puzzle
- * 5-gattai puzzles and generalisations thereof
- * analyze where pose mode spends its time.
+/*
+ *  sku - analysis tool for Sudoku puzzles
+ *  Copyright (C) 2005  Richard P. Curnow
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
 #include <time.h>
